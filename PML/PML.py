58648cf77c27daa91d5689bc516ef7166e9656bd
LukeM <lukechile@gmail.com>
2017-11-01 20:26:04 +0100

    Back-prop and Visualiztion
    made changes to forward and backprop such that they are now access my a method that calls the corresponding methods after each other. added a visualization of the decision boundaries

--- PML/PML.py
+++ PML/PML.py
@@ -5,6 +5,7 @@
 # Project by: Luke Martin
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Network:
@@ -17,26 +18,57 @@ class Network:
     def add_layer(self, layer):
         self.layers.append(layer)
 
-    def forward_propagate(self, input_data, Y):
+    def forward_propagate(self, input_data):
         A = input_data
-        L = len(self.network.layers)
-        for l in range(L - 1):
-            self.layers[l].linear_forward(A)
-            A = self.layers[l].sigmoid_forward()
-        cost = self.compute_cost(Y, A)
-        return cost
-
-    def back_propogate(self, labels):
         L = len(self.layers)
-        dA = None
+        for l in range(L):
+            # print("Layer: ", l)
+            A = self.layers[l].forward_propagate(A)
+            # print("Z" + str(l + 1) + "= ", Z)
+            # print("A" + str(l + 1) + "= ", A)
+
+        return A
+
+
+
+    def back_propagate(self, labels, AL):
+        L = len(self.layers)
+        dA = -1 * (np.divide(labels, AL) - np.divide(1 - labels, 1 - AL))
+        dZ = AL - labels
+        dA = self.layers[L - 1].linear_backward(dZ)
         for l in reversed(range(L - 1)):
-            if l is L-1:
-                AL = self.layers[l].A
-                # TODO: Update this for the output layer
-                dA =  (np.divide(labels, AL) - np.divide(1 - layers, 1 - AL))
-            else:
-                dZ = self.layers[l].sigmoid_backwards(dA) # Requires dA
-                dA = self.layers[l].linear_backward(dZ) # Require dZ
+            self.layers[l].back_propagate(dA)
+            # dZ = self.layers[l].sigmoid_backward(dA) # Requires dA
+            # _,_, dA = self.layers[l].linear_backward(dZ)  # Require dZ
+
+    def predict(self, input_data):
+        AL = self.forward_propagate(input_data)
+        prediction = 1*(AL > 0.5)
+        return prediction
+
+    def plot_decision_boundary(self, X, y):
+        # Set min and max values and give it some padding
+        x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+        y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+
+        print("x min/max = ", x_min, x_max)
+        print("y min/max = ", y_min, y_max)
+        h = 0.01
+        # Generate a grid of points with distance h between them
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+        print("xx shape = ", xx.shape)
+        print("yy shape = ", yy.shape)
+
+        mesh_points = np.array([xx.ravel(), yy.ravel()])
+
+        # Predict the function value for the whole gid
+        Z = self.predict(mesh_points)
+        Z = Z.reshape(xx.shape)
+        # Plot the contour and training examples
+        plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+        plt.show()
 
 
 class Optimizer:
@@ -45,16 +77,17 @@ class Optimizer:
     """
     def __init__(self, net):
         self.network = net
-        self.learning_rate = None
+        self.learning_rate = 1.2
 
-
-    def train(self, input_data, labels, iterations=100):
+    def train(self, input_data, labels, iterations=10000):
         for i in range(iterations):
-            cost = self.network.forward_propagate(input_data, labels)
-            self.network.back_propogate()
-            self.update_parameters()
 
-            print(cost)
+            AL = self.network.forward_propagate(input_data)
+            cost = self.compute_cost(labels, AL)
+            self.network.back_propagate(labels, AL)
+            self.update_parameters()
+            if i % 1000 is 0:
+                print("iteration: ",i," cost = ", cost)
 
     def compute_cost(self, Y, AL):
         """
@@ -65,8 +98,8 @@ class Optimizer:
         """
         # TODO: Update this to be more versatile
         m = Y.shape[1]
-        Y = Y.reshape(AL.shape)
-        cost = -(1 / m) * np.sum(np.multiply(np.log(AL), Y) + np.multiply(np.log(1 - AL), (1 - Y)))
+        logprobs = np.multiply(np.log(AL), Y) + np.multiply(np.log(1 - AL), (1 - Y))
+        cost = -1 * (1 / m) * np.sum(logprobs)
         cost = np.squeeze(cost)
         assert (cost.shape == ())
 
@@ -74,16 +107,17 @@ class Optimizer:
 
     def update_parameters(self):
         L = len(self.network.layers)
-        for l in range(L-1):
+        for l in range(L):
             self.network.layers[l].W -= self.network.layers[l].dW * self.learning_rate
             self.network.layers[l].b -= self.network.layers[l].db * self.learning_rate
 
 
 class Layer:
-    def __init__(self):
+    def __init__(self, act_func):
         self.n_x = None
         self.n_h = None
-        self.n_h = None
+
+        self.act_func = act_func
 
         self.layer_type = None
 
@@ -102,7 +136,7 @@ class Layer:
 
         self.activation_function = None
 
-    def initialize(self, n_x, n_h, n_y, layer_type = "fc"):
+    def initialize(self, n_x, n_h, layer_type="fc"):
         """
         :param n_x: size of input layer
         :param n_h: size of hidden layer
@@ -111,7 +145,6 @@ class Layer:
         """
         self.n_x = n_x
         self.n_h = n_h
-        self.n_h = n_y
 
         self.layer_type = layer_type
 
@@ -121,25 +154,38 @@ class Layer:
         assert (self.W.shape == (n_h, n_x))
         assert (self.b.shape == (n_h, 1))
 
+        return self.W, self.b
+
     def linear_forward(self, input_A):
         self.A_prev = input_A
-        self.Z = np.dot(self.W.T, input_A) + self.b
+        # print("W shape = ", self.W.shape)
+        # print("Input shape = ", input_A.shape)
+        self.Z = np.dot(self.W, input_A) + self.b
+        # print("Output shape =  ", self.Z.shape)
         return self.Z
 
     def relu_forward(self):
         return
 
-    def sigmoid_forward(self):
-        self.A = 1/(1 + np.exp(-self.Z))
+    def relu_backward(self):
+        return
+
+    def tanh_forward(self, Z):
+        self.A = np.tanh(Z)
+        return self.A
+
+    def tanh_backward(self, dA):
+        self.dZ = dA * (1 - np.power(self.A, 2))
+        return self.dZ
+
+    def sigmoid_forward(self, Z):
+        self.A = 1/(1 + np.exp(-Z))
         return self.A
 
     def sigmoid_backward(self, dA):
         sig_deriv = np.exp(self.Z) / np.power((np.exp(self.Z) + 1), 2)
         self.dZ = dA * sig_deriv
         return self.dZ
-
-    def relu_backward(self):
-        return
 
     def linear_backward(self, dZ):
         """
@@ -158,7 +204,34 @@ class Layer:
 
         self.dW = (1 / m) * np.dot(dZ, self.A_prev.T)
         self.db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
+        # print("dW = ", self.dW)
+        # print("db = ", self.db)
+
         dA_prev = np.dot(self.W.T, dZ)
 
-        return self.dW, self.db, dA_prev
+        return dA_prev, self.dW, self.db
 
+    def back_propagate(self, dA):
+        dZ = None
+        if self.act_func is "sigmoid":
+            dZ = self.sigmoid_backward(dA)
+        elif self.act_func is "relu":
+            dZ = self.relu_backward()
+        elif self.act_func is "tanh":
+            dZ = self.tanh_backward(dA)
+
+        dA_prev, dW, db = self.linear_backward(dZ)
+        return dA_prev
+
+    def forward_propagate(self, prev_A):
+        Z = self.linear_forward(prev_A)
+        A = None
+        if self.act_func is "tanh":
+            A = self.tanh_forward(Z)
+        elif self.act_func is "sigmoid":
+            A = self.sigmoid_forward(Z)
+        elif self.act_func is "relu":
+            A = self.relu_forward()
+
+        return A
--- PML/test.py
+++ PML/test.py
@@ -1,21 +1,41 @@
 import os.path
 import sys
+import sklearn
+import sklearn.datasets
 import numpy as np
+import matplotlib.pyplot as plt
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import PML as ml
+
 
 def linear_forward_test1():
     test_layer = ml.Layer()
 
-    test_layer.A = np.array([[1.62434536, -0.61175641],
+    input_A = np.array([[1.62434536, -0.61175641],
                              [-0.52817175, -1.07296862],
                              [0.86540763, -2.3015387]])
+
     test_layer.W = np.array([1.74481176, -0.7612069, 0.3190391])
     test_layer.b = np.array([-0.24937038])
 
-    print(test_layer.linear_forward())
+    print(test_layer.linear_forward(input_A))
 
-    print(test_layer.sigmoid_forward())
+
+def sigmoid_forward_test1():
+    test_layer = ml.Layer()
+
+    input_A = np.array([[-0.41675785, -0.05626683],
+                        [-2.1361961,   1.64027081],
+                        [-1.79343559, -0.84174737]])
+
+    test_layer.W = np.array([ 0.50288142, -1.24528809, -1.05795222])
+    test_layer.b = np.array([-0.90900761])
+
+    Z = test_layer.linear_forward(input_A)
+
+    A = test_layer.sigmoid_forward(Z)
+    print("A = ", A)
 
 
 def linear_backward_test1():
@@ -27,11 +47,12 @@ def linear_backward_test1():
                                   [ 0.86540763, -2.3015387 ],
                                   [ 1.74481176, -0.7612069 ]])
 
-    dW, db, dA_prev = test_layer.linear_backward(np.array([1.62434536, -0.61175641], ndmin=2))
+    dZ = np.array([1.62434536, -0.61175641], ndmin=2)
+    dW, db, dA_prev = test_layer.linear_backward(dZ)
 
-    #TODO: Copy above code and make a proper test_layer constructor
+    # TODO: Copy above code and make a proper test_layer constructor
 
-    print("dA_prev = ", str(dA_prev))
+    print("dA_prev = ", dA_prev)
     print("dW = ", str(dW))
     print("db = ", str(db))
 
@@ -48,11 +69,201 @@ def sigmoid_backward_test1():
     print("[Truth] dZ  =  [[-0.10414453 -0.01044791]]")
 
 
-def main():
+def model_test1():
+    net = ml.Network()
 
-    #linear_forward_test1()
-    #linear_backward_test1()
-    sigmoid_backward_test1()
+    layer1 = ml.Layer("tanh")
+    layer1.initialize(2, 3)
+
+    layer2 = ml.Layer("sigmoid")
+    layer2.initialize(3, 1)
+
+    layer3 = ml.Layer("sigmoid")
+    layer3.initialize(3,1)
+
+    # print("L1 W = ", str(layer1.W))
+    # print("L1 b = ", str(layer1.b))
+
+    net.add_layer(layer1)
+    net.add_layer(layer2)
+    #net.add_layer(layer3)
+
+    np.random.seed(3)
+    X, y = sklearn.datasets.make_moons(200, noise=0.20)
+    plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
+    X_n = np.array(X).T
+    y_n = np.array(y, ndmin=2)
+    # print("y shape = ", y.shape)
+    # print("X shape = ", X.shape)
+    # plt.show()
+    # net.plot_decision_boundary(X, y)
+
+    opt = ml.Optimizer(net)
+    opt.train(X_n,y_n)
+
+    net.plot_decision_boundary(X, y)
+
+
+    prediction = net.predict(X_n)
+
+    sum(prediction - y)
+
+
+def layer_initialize_test1():
+    np.random.seed(1)
+    layer = ml.Layer()
+
+    parameters = layer.initialize(3, 2, 1, layer_type="fc")
+
+    print(parameters)
+
+
+def forward_prop_test1():
+    layer1 = ml.Layer()
+    layer2 = ml.Layer()
+    layer3 = ml.Layer()
+
+    layer1.W = np.array(
+        [[0.35480861,  1.81259031, -1.3564758, -0.46363197,  0.82465384],
+         [-1.17643148,  1.56448966,  0.71270509, -0.1810066,   0.53419953],
+         [-0.58661296, -1.48185327,  0.85724762,   0.94309899,  0.11444143],
+         [-0.02195668, -2.12714455, -0.83440747, - 0.46550831,  0.23371059]]
+    )
+
+    layer1.b = np.array([[ 1.38503523],
+                         [-0.51962709],
+                         [-0.78015214],
+                         [ 0.95560959]])
+
+    layer2.W = np.array(
+        [[-0.12673638, -1.36861282,  1.21848065, -0.85750144],
+         [-0.56147088, -1.0335199,   0.35877096,  1.07368134],
+         [-0.37550472,  0.39636757, -0.47144628,  2.33660781]])
+
+    layer2.b = np.array([[ 1.50278553],
+                        [-0.59545972],
+                        [ 0.52834106]])
+
+    layer3.W = np.array([[ 0.9398248, 0.42628539, -0.75815703]])
+    layer3.b = np.array([-0.16236698])
+
+    net = ml.Network()
+
+    net.add_layer(layer1)
+    net.add_layer(layer2)
+    net.add_layer(layer3)
+
+    X = np.array([[-0.31178367,  0.72900392,  0.21782079, -0.8990918 ],
+                  [-2.48678065,  0.91325152,  1.12706373, -1.51409323],
+                  [ 1.63929108, -0.4298936,   2.63128056,  0.60182225],
+                  [-0.33588161,  1.23773784,  0.11112817,  0.12915125],
+                  [ 0.07612761, -0.15512816,  0.63422534,  0.810655  ]])
+
+
+    test = np.dot(layer1.W, X) + layer1.b
+    print("test = ", test)
+    AL = net.forward_propagate(X)
+
+
+def cost_function_test1():
+    print("test")
+    net = ml.Network()
+    opt = ml.Optimizer(net)
+
+
+    Y = np.array([1, 1, 1], ndmin=2)
+    AL = np.array([ 0.8,  0.9, 0.4])
+
+    cost = opt.compute_cost(Y, AL)
+
+    print(cost)
+
+
+def full_backprop_test1():
+    print("Full backprop test1")
+
+
+    labels = np.array([ True, False, True] , ndmin=2)
+    X = np.array([[ 1.62434536, -0.61175641, -0.52817175],
+                  [-1.07296862,  0.86540763, -2.3015387 ]])
+
+    net = ml.Network()
+    layer1 = ml.Layer("tanh")
+    layer2 = ml.Layer("sigmoid")
+
+    layer1.W = np.array([[-0.00416758, -0.00056267],
+                         [-0.02136196,  0.01640271],
+                         [-0.01793436, -0.00841747],
+                         [ 0.00502881, -0.01245288]])
+    layer1.b = np.zeros((4,1))
+    layer1.A = np.array( [[-0.00616578,  0.0020626  , 0.00349619],
+                          [-0.05225116  ,0.02725659 ,-0.02646251],
+                          [-0.02009721  ,0.0036869  ,0.02883756],
+                          [ 0.02152675 ,-0.01385234 ,0.02599885]])
+
+    layer2.W = np.array([-0.01057952, -0.00909008,  0.00551454,  0.02292208], ndmin=2)
+    layer2.b = np.zeros((1, 1))
+    layer2.A = np.array([0.5002307, 0.49985831, 0.50023963], ndmin=2)
+    layer2.A_prev = layer1.A
+    layer1.A_prev = X
+
+    net = ml.Network()
+    net.add_layer(layer1)
+    net.add_layer(layer2)
+
+    AL = layer2.A
+    net.back_propagate(labels, AL)
+
+    print("[Output] dW1 = ", layer1.dW)
+    print("[Output] db1 = ", layer1.db)
+
+    print("[Output] dW2 = ", layer2.dW)
+    print("[Output] db1 = ", layer2.db)
+
+    print("[Truth] dW1 = [[ 0.00301023 -0.00747267] \n [ 0.00257968 -0.00641288]\n [-0.00156892  0.003893  ] \n [-0.00652037  0.01618243]]")
+    print("[Truth] db1 = \n [[ 0.00176201] \n [ 0.00150995] \n  [-0.00091736] \n [-0.00381422]]")
+
+    print("[Truth] dW2 = [[ 0.00078841  0.01765429 -0.00084166 -0.01022527]]")
+    print("[Truth] db2 = [[-0.16655712]]")
+
+
+def model_test_2():
+    np.random.seed(3)
+    X = np.array([[ 1.62434536, -0.61175641, -0.52817175],
+                  [-1.07296862,  0.86540763, -2.3015387]])
+    y = np.array([True, False, True], ndmin=2)
+    net = ml.Network()
+
+    layer1 = ml.Layer("tanh")
+    layer1.initialize(2, 4)
+    layer2 = ml.Layer("sigmoid")
+    layer2.initialize(4, 1)
+
+    net.add_layer(layer1)
+    net.add_layer(layer2)
+
+    opt = ml.Optimizer(net)
+
+    opt.train(X, y, iterations=10000)
+
+    print("W1 = ", layer1.W)
+    print("b1 = ", layer1.b)
+
+    print("W2 = ", layer2.W)
+    print("b2 = ", layer2.b)
+
+def main():
+    print("Running test  case:")
+    # linear_forward_test1()
+    # sigmoid_forward_test1()
+    # linear_backward_test1()
+    # sigmoid_backward_test1()
+    # model_test1()
+    # model_test_2()
+    # layer_initialize_test1()
+    # forward_prop_test1()
+    # cost_function_test1()
+    full_backprop_test1()
     #TODO: write a compute cost test
 
 
